58bc68e31c82ddf8825936fdef86f98385620318
JosipHorvat <josiph988@gmail.com>
2021-02-08 12:36:47 +0100

    Everything for "show ingredient"

---
+++ src/test/java/guru/springframework/services/IngredientServiceImplTest.java
@@ -0,0 +1,17 @@
+package guru.springframework.services;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IngredientServiceImplTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    public void findRecipeIdAndIngredientId() {
+    }
+}
